fd3dd83a297936d872368db8923c612fd6f6b03d
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-08-14 15:50:37 +0800

    added merge algo

--- merge-sort.js
+++ merge-sort.js
@@ -1,6 +1,43 @@
 console.log("merge sort");
 const merge = (arr1, arr2) => {
-  return [...arr1, ...arr2];
+  let i = 0;
+  let j = 0;
+
+  const result = [];
+  while (i < arr1.length || j < arr2.length) {
+    if (i >= arr1.length) {
+      result.push(arr2[j]);
+      j++;
+
+      continue;
+    }
+
+    if (j >= arr2.length) {
+      result.push(arr1[i]);
+      i++;
+
+      continue;
+    }
+
+    const cur1 = arr1[i];
+    const cur2 = arr2[j];
+
+    if (cur1 === cur2) {
+      result.push(cur1);
+      result.push(cur2);
+
+      i++;
+      j++;
+    } else if (cur1 < cur2) {
+      result.push(cur1);
+      i++;
+    } else {
+      result.push(cur2);
+      j++;
+    }
+  }
+
+  return result;
 };
 
 const mergeSort = (arr) => {
