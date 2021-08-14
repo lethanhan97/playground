4d5367839d63c63989166f170ffbb0b335b9f319
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-08-14 17:07:56 +0800

    added karatsuba method

--- multiplication.js
+++ multiplication.js
@@ -30,10 +30,18 @@ const multiply = (x, y) => {
   return result;
 };
 
+const karatsuba = (x, y) => {
+  if (x < 10 && y < 10) return x * y;
+
+  // Todo fill in method
+  return x * y;
+};
+
 const testCases = [
-  [1023, 3134],
-  [134, 1230],
-  [12, 103123],
+  [
+    3141592653589793238462643383279502884197169399375105820974944592,
+    2718281828459045235360287471352662497757247093699959574966967627,
+  ],
 ];
 
 testCases.forEach((testCase) => {
